7848c3009181fbc4ce7b4c9b342a42780b4c1c1b
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2024-12-19 20:45:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        for(int i=0;i<arr.length();i++){
+            sort(arr1,arr1+i);
+            if(arr1(i))
+        }
+    }
+};
