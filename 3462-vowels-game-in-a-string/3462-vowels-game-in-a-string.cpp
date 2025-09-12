d79bf18906df6a694fe7cd7d23e0e5477335da03
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2025-09-12 13:51:48 +0530

    Time: 11 ms (34.33%), Space: 19.2 MB (25.75%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        string t="aeiouAEIOU";
+        for(int i=0; i<s.size(); i++) if(t.find(s[i])!=string::npos) return true;
+        return false;
+    }
+};
