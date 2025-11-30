57707efa98c0d013ac466a1bd42dcc4f4cadb62e
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2025-11-30 18:55:39 +0530

    Time: 74 ms (75%), Space: 87.7 MB (74.34%) - LeetHub

--- 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int minSubarray(vector<int>& nums, int p) {
         int rem=0;
-        for(int i: nums)
-            rem = (rem+i)%p;
-        if( rem == 0 )
-            return 0;
+        for(int i: nums) rem = (rem+i)%p;
+        if(rem == 0) return 0;
         unordered_map<int,int> hash;
         hash[0] = -1;
-        int len = nums.size();
-        int ans = len;
-        int curr = 0;
-        for(int i=0; i<len; ++i){
+        int len = nums.size(), ans = len, curr = 0;
+        for(int i=0; i < len; ++i){
             curr = (curr + nums[i])%p;
             int target = (curr - rem + p) % p;
             if(hash.count(target)){
-                ans = min(ans,i-hash[target]);
+                ans = min(ans, i - hash[target]);
             }
             hash[curr] = i;
         }
-        return ans == len?-1:ans;
+        return ans == len? -1: ans;
     }
 };
