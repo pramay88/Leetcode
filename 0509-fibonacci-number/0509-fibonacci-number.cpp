99604c106755575aa6e89c707e1fb82a543dd101
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2025-12-01 22:13:44 +0530

    Time: 2 ms (76.46%), Space: 7.8 MB (64.66%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,13 @@
+    class Solution {
+    public:
+        int fib(int n) {
+            if(n == 0) return 0;
+            else if(n == 1) return 1;
+            int a = 0, b = 1, temp;
+            for(int i = 2; i <= n; i++){
+                temp = a + b;
+                a = b, b = temp;
+            }
+            return b;
+        }
+    };
