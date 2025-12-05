c87950b99bd594e548f5e5aa2dbac1f0d18fb160
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2025-12-05 23:59:06 +0530

    Time: 0 ms (100%), Space: 23.3 MB (5.3%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPartitions(vector<int>& nums) {
-        int count = 0, n = nums.size();
+        int n = nums.size();
         vector<int> prefixSum(n, 0);
         prefixSum[0] = nums[0];
         
@@ -10,10 +10,6 @@ public:
         
         if(prefixSum[n - 1] % 2 != 0) return 0;
         
-        for(int i = 0; i < n - 1; i++){
-            if((prefixSum[n - 1] - 2 * prefixSum[i]) % 2 == 0)
-                count++;
-        }
-        return count;
+        return n - 1;
     }
 };
