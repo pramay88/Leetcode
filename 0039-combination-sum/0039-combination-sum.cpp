13fc15f026d7b329b35a915c871376cc7428b1b9
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2025-12-25 23:19:19 +0530

    Time: 0 ms (100%), Space: 14 MB (73.56%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    void findCombinations(int i, vector<int> &arr, int target, vector<vector<int>> &ans, vector<int> &ds){
-        if(i == arr.size()){
-            if(target == 0) ans.push_back(ds);
-            return;
+    void subsequence(int i, vector<int> &arr, vector<int> &sub, vector<vector<int>> &ans, int target){
+        if(i >= arr.size()){
+            if(target == 0) ans.push_back(sub);
+            return; 
         }
         if(arr[i] <= target){
-            ds.push_back(arr[i]);
-            findCombinations(i, arr, target - arr[i], ans, ds);
-            ds.pop_back();
+            sub.push_back(arr[i]);
+            subsequence(i, arr, sub, ans, target - arr[i]);
+            sub.pop_back();
         }
-        findCombinations(i+1, arr, target, ans, ds);
+        subsequence(i + 1, arr, sub, ans, target);
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        findCombinations(0, candidates, target, ans, ds);
-        return ans;
+        vector<int> sub;    
+        vector<vector<int>> res;
+        subsequence(0, candidates, sub, res, target);
+        return res;
     }
 };
