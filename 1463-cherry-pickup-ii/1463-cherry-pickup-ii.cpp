b052d272a76a613e290f5a7fd0d4810cff909c8d
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-01-05 14:10:43 +0530

    Time: 251 ms (5.06%), Space: 67.7 MB (5.17%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,32 +1,36 @@
 class Solution {
-    int dp[71][71][71];
-    int n, m;
-
-    int solve(vector<vector<int>> &grid, int row, int j1, int j2){
-        if(j1 < 0 || j1 >= m || j2 < 0 || j2 >= m) return -1e8;
-
-        if(dp[row][j1][j2] != -1) return dp[row][j1][j2];
-
-        if(row == n - 1){
-            if(j1 == j2) return dp[row][j1][j2] =  grid[row][j1];
-            else return dp[row][j1][j2] = grid[row][j1] + grid[row][j2];
-        }
-
-        vector<int> dir = {-1, 0, 1};
-        int maxx = -1e8;
-        for(int x: dir){
-            for(int y: dir){
-                int cherries = (j1 == j2)? grid[row][j1]: grid[row][j1] + grid[row][j2];
-                cherries += solve(grid, row + 1, j1 + x, j2 + y);
-                maxx = max(maxx, cherries);
-            }
-        }
-        return dp[row][j1][j2] = maxx;
-    }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        n = grid.size(), m = grid[0].size();
-        memset(dp, -1, sizeof(dp));
-        return solve(grid, 0, 0, m - 1);
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(m, vector<int>(m, -1))
+        );
+
+        for(int j1 = 0; j1 < m; j1++){
+            for(int j2 = 0; j2 < m; j2++){
+                if(j1 == j2) dp[n - 1][j1][j2] =  grid[n - 1][j1];
+                else dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+            }
+        }
+
+        for(int row = n - 2; row >= 0; row--){
+            for(int j1 = 0; j1 < m; j1++){
+                for(int j2 = 0; j2 < m; j2++){
+                    vector<int> dir = {-1, 0, 1};
+                    int maxx = -1e8;
+                    for(int x: dir){
+                        for(int y: dir){
+                            int cherries = (j1 == j2)? grid[row][j1]: grid[row][j1] + grid[row][j2];
+                            if(j1 + x >= 0 && j1 + x < m && j2 + y >= 0 && j2 + y < m)
+                                cherries += dp[row + 1][j1 + x][j2 + y];
+                            else cherries += -1e8;
+                            maxx = max(maxx, cherries);
+                        }
+                    }
+                    dp[row][j1][j2] = maxx;
+                }
+            }
+        }
+        return dp[0][0][m-1];
     }
 };
