fdcfc1ff38d154ce09d11c5a05972a6c5110aa36
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-01-05 14:14:22 +0530

    Time: 239 ms (5.06%), Space: 67.7 MB (5.17%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int cherryPickup(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(m, -1))
-        );
+        vector<vector<int>> dp(m, vector<int>(m, -1));
 
         for(int j1 = 0; j1 < m; j1++){
             for(int j2 = 0; j2 < m; j2++){
-                if(j1 == j2) dp[n - 1][j1][j2] =  grid[n - 1][j1];
-                else dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+                if(j1 == j2) dp[j1][j2] =  grid[n - 1][j1];
+                else dp[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
             }
         }
 
         for(int row = n - 2; row >= 0; row--){
+            vector<vector<int>> curr(m, vector<int>(m, -1));
             for(int j1 = 0; j1 < m; j1++){
                 for(int j2 = 0; j2 < m; j2++){
                     vector<int> dir = {-1, 0, 1};
@@ -22,15 +21,16 @@ public:
                         for(int y: dir){
                             int cherries = (j1 == j2)? grid[row][j1]: grid[row][j1] + grid[row][j2];
                             if(j1 + x >= 0 && j1 + x < m && j2 + y >= 0 && j2 + y < m)
-                                cherries += dp[row + 1][j1 + x][j2 + y];
+                                cherries += dp[j1 + x][j2 + y];
                             else cherries += -1e8;
                             maxx = max(maxx, cherries);
                         }
                     }
-                    dp[row][j1][j2] = maxx;
+                    curr[j1][j2] = maxx;
                 }
             }
+            dp = curr;
         }
-        return dp[0][0][m-1];
+        return dp[0][m-1];
     }
 };
