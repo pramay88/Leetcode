43cc0da9fac107457b1824cae83879b3920f82a8
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-01-08 11:38:46 +0530

    Time: 8 ms (91.6%), Space: 17.3 MB (69.6%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size(), m = nums2.size();
-        vector<vector<int>> dp(n, vector<int>(m, INT_MIN));
+        vector<int> dp(m, INT_MIN);
 
         for(int i = 0; i < n; i++){
+            vector<int> next(m, INT_MIN);
+
             for(int j = 0; j < m; j++){
                 int curr = nums1[i] * nums2[j];
                 int ans = curr; // start fresh
 
                 if(i > 0 && j > 0)
-                    ans = max(ans, curr + dp[i - 1][j - 1]); // extend
+                    ans = max(ans, curr + dp[j - 1]); // extend
 
                 if(i > 0)
-                    ans = max(ans, dp[i - 1][j]); // skip nums1[i]
+                    ans = max(ans, dp[j]); // skip nums1[i]
 
                 if(j > 0)
-                    ans = max(ans, dp[i][j - 1]); // skip nums2[j]
+                    ans = max(ans, next[j - 1]); // skip nums2[j]
 
-                dp[i][j] = ans;
+                next[j] = ans;
             }
+            dp = next;
         }
-        return dp[n - 1][m - 1];
+        return dp[m - 1];
     }
 };
