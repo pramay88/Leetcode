4adea1539e8825f33c5dea307e6ac2e8a556570d
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-01-09 15:27:26 +0530

    Time: 0 ms (100%), Space: 18.8 MB (21.79%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    void makeZero(vector<vector<int>> & mat, int x, int y){
-        int n = mat.size(), m = mat[0].size();
-        for(int j = 0; j < m; j++) mat[x][j] = 0;
-        for(int i = 0; i < n; i++) mat[i][y] = 0;
-    }
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size(), m = matrix[0].size();
-        vector<vector<int>> hash(n, vector<int>(m, -1));
+        vector<int> row(m, -1), col(n, -1);
         
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
-                if(matrix[i][j] == 0) hash[i][j] = 0;
+                if(matrix[i][j] == 0) col[i] = 0, row[j] = 0;
         
         for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-                if(hash[i][j] == 0) makeZero(matrix, i, j);
-            
+            if(col[i] == 0)
+                for(int j = 0; j < m; j++) matrix[i][j] = 0;
+
+        for(int j = 0; j < m; j++)
+            if(row[j] == 0)
+                for(int i = 0; i < n; i++) matrix[i][j] = 0;            
     }
 };
