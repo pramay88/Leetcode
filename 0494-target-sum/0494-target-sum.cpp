40a2a75dc462f52fe4a1591474a704bd86239411
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-01-09 19:48:07 +0530

    Time: 4 ms (77.09%), Space: 15.2 MB (57.31%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,31 @@
 class Solution {
-    int countSubsets(vector<int>& arr, int n, int sum){
-        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
-        dp[0][0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= sum; j++) {
-                dp[i][j] = dp[i-1][j];
-                if (arr[i-1] <= j)
-                    dp[i][j] += dp[i-1][j - arr[i-1]];
-            }
-        }
-        return dp[n][sum];
-    }
-
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size(), sum = 0;
+        for(int &n: nums) sum += n;
+        
+        if(sum < abs(target) || (sum - target) % 2 != 0) return 0;
+        
+        int t = (sum - target) / 2;
+        vector<vector<int>> dp(n, vector<int>(t + 1, 0));
+        
+        //base case
+        if(nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
 
-        if (abs(target) > sum) return 0;
-        if ((sum + target) % 2) return 0;
+        if (nums[0] != 0 && nums[0] <= t)
+        dp[0][nums[0]] = 1;
 
-        int reqSum = (sum + target) / 2;
-        return countSubsets(nums, n, reqSum);
+
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= t; j++){
+                int take = 0;
+                if(nums[i] <= j) take = dp[i - 1][j - nums[i]];
+                int notTake = dp[i - 1][j];
+                dp[i][j] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][t];
     }
 };
