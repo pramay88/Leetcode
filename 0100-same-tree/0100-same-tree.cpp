5c6865428b0dea7904b0d93f714b61f0c059119f
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-02-10 10:23:24 +0530

    Time: 0 ms (100%), Space: 12.9 MB (12.87%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,24 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//  Check the Node and it left and right Subtrees
 class Solution {
-    void preOrder(TreeNode * node, vector<int> & res){
-        if(!node){
-            res.push_back(INT_MIN);
-            return;
-        }
-        res.push_back(node->val);
-        preOrder(node->left, res);
-        preOrder(node->right, res);
-    }
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int> res1, res2;
-        preOrder(p, res1);
-        preOrder(q, res2);
-        for(int i = 0; i < res1.size(); i++){
-            if(res1[i] != res2[i]) return false;
-        }
-        return true;
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);    
     }
 };
