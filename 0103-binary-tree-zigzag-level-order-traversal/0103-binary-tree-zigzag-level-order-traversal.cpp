dacc2a0d4fea826db22924d1ae7ad0ab2b799eb8
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-02-10 18:54:38 +0530

    Time: 0 ms (100%), Space: 15.7 MB (5.71%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -10,36 +10,32 @@
  * };
  */
 class Solution {
-    vector<vector<int>> res;
-    void traverse(TreeNode * root){
-        if (!root) return;
-        queue<TreeNode*> q;
-        q.push(root);
-        bool reversal = false;
-        
-        while(!q.empty()){
-            vector<int> level;
-            int levelSize = q.size();
-            
-            for(int i = 0; i < levelSize; i++){
-                TreeNode * node = q.front();
-                q.pop();
-                
-                level.push_back(node->val);
-            
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
-                }
-            if(reversal){
-                reverse(level.begin(), level.end());
-            }
-            res.push_back(level);
-            reversal = !reversal;
-        }
-    }
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        traverse(root);
+        vector<vector<int>> res;
+        if(!root) return res;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int leftToRight = true;
+
+        while(!q.empty()){
+            int levelSize = q.size();
+            deque<int> dq;
+
+            for(int i = 0; i < levelSize; i++){
+                TreeNode * node = q.front(); q.pop();
+                
+                if(leftToRight) dq.push_back(node->val);
+                else dq.push_front(node->val);
+
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            res.emplace_back(dq.begin(), dq.end());
+            leftToRight = !leftToRight;
+        }
         return res;
     }
 };
