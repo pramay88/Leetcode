70300df52901d3c21073950a5069ab3c2a8204ac
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-02-12 22:30:08 +0530

    Time: 47 ms (96.24%), Space: 25.3 MB (85.06%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        int n = s.size(), maxLen = 0;
+
+        for(int i = 0; i < n; i++) {
+            vector<int> freq(26, 0);
+            int distinct = 0, maxFreq = 0;
+
+            for(int j = i; j < n; j++) {
+                if(freq[s[j] - 'a'] == 0)
+                    distinct++;
+
+                freq[s[j] - 'a']++;
+                maxFreq = max(maxFreq, freq[s[j] - 'a']);
+
+                int len = j - i + 1;
+
+                if(len == distinct * maxFreq)
+                    maxLen = max(maxLen, len);
+            }
+        }
+        return maxLen;
+    }
+};
