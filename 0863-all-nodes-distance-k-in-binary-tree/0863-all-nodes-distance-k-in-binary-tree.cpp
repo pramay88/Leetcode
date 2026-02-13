8c659d047129be995181f18b7882550d10f64354
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-02-13 22:15:16 +0530

    Time: 3 ms (81.66%), Space: 15.6 MB (25.16%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> kNodes;
+        if(!root) return kNodes;
+
+        map<TreeNode*, TreeNode*> parent;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            TreeNode * node = q.front();
+            q.pop();
+            TreeNode *left = NULL, *right = NULL;
+            if(node->left) left = node->left, parent[left] = node;
+            if(node->right) right = node->right, parent[right] = node;
+            if(left) q.push(left);
+            if(right) q.push(right);
+        }
+
+        queue<pair<TreeNode*, int>> qq;
+        unordered_map<TreeNode*, int> vis;
+        qq.push({target, 0});
+        
+        while(!qq.empty()){
+            auto [node, dist] = qq.front();
+            qq.pop();
+
+            if(vis[node] >= 1) continue;
+            vis[node]++;
+            
+            if(dist == k){
+                kNodes.push_back(node->val);
+                continue;
+            }
+
+            if(node->left) qq.push({node->left, dist + 1});
+            if(node->right) qq.push({node->right, dist + 1});
+            if(parent[node]) qq.push({parent[node], dist + 1});
+        }
+        return kNodes;
+    }
+};
