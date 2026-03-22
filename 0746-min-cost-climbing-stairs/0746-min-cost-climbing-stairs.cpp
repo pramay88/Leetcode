d63eb21f1247e65db4b2f8c235d974b983fb3628
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-03-22 14:05:04 +0530

    Time: 0 ms (100%), Space: 17.8 MB (11.53%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,13 @@
-// dp[i] = min cost to reach step i
+// dp[i] = min cost to reach step i (without including cost of i)
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n);
+        vector<int> dp(n + 1);
 
-        dp[0] = cost[0], dp[1] = cost[1];
+        for(int i = 2; i <= n; i++)
+            dp[i] = min(cost[i - 1] + dp[i - 1], cost[i - 2] + dp[i - 2]);
 
-        for(int i = 2; i < n; i++)
-            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
-
-        return min(dp[n - 1], dp[n - 2]);
+        return dp[n];
     }
 };
