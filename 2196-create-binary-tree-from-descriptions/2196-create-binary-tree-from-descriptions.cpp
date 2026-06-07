a660f3f6f9b14109932fbbf4b819412c7bd416f3
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-06-07 21:39:45 +0530

    Time: 177 ms (81.05%), Space: 279.2 MB (59.54%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> nodes;
+        unordered_set<int> children;
+
+        for (auto& d : descriptions){
+            int parent = d[0], child = d[1], isLeft = d[2];
+
+            if(!nodes.count(parent))
+                nodes[parent] = new TreeNode(parent);
+
+            if(!nodes.count(child))
+                nodes[child] = new TreeNode(child);
+
+            if(isLeft)
+                nodes[parent]->left = nodes[child];
+            else
+                nodes[parent]->right = nodes[child];
+
+            children.insert(child);
+        }
+
+        for(auto& [val, node] : nodes){
+            if(!children.count(val))
+                return node;
+        }
+
+        return nullptr;
+    }
+};
