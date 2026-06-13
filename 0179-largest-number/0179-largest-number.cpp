067cf1fea4f922ea3848084a26a6325678739705
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-06-13 12:03:07 +0530

    Time: 3 ms (75.32%), Space: 17.1 MB (57.25%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -3,22 +3,19 @@ public:
     string largestNumber(vector<int>& nums) {
         int n = nums.size();
         vector<string> elements(n);
-        for (int i = 0; i < n; i++) {
-            elements[i] = to_string(nums[i]);
-        }
 
-        sort(elements.begin(), elements.end(), [](const string& a, const string& b) {
-            return (b + a) < (a + b);
+        for(int i = 0; i < n; i++)
+            elements[i] = to_string(nums[i]);
+
+        sort(elements.begin(), elements.end(), [](const string& a, const string& b){
+            return (a + b) > (b + a);
         });
 
-        if (elements[0] == "0") {
-            return "0";
-        }
+        if(elements[0] == "0") return "0";
 
         string result;
-        for (const string& val : elements) {
+        for(const string& val : elements)
             result += val;
-        }
 
         return result;
     }
