f2da2a4bff6269de095a9f81e455b92c67e4a8f2
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-06-14 20:31:11 +0530

    Time: 3 ms (45.87%), Space: 10 MB (11.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,15 +3,15 @@ public:
     bool isAnagram(string s, string t) {
         unordered_map<char, int> hash;
         if(s.length() != t.length()) return false;
-        for (char c : s) {
-            hash[c]++;
-        }
-        for (char c : t) {
+
+        for(char c : s) hash[c]++;
+        
+        for(char c : t){
             hash[c]--;
-            if (hash[c] == 0) {
+            if(hash[c] == 0)
                 hash.erase(c);
-            }
         }
+        
         return hash.empty();
     }
 };
