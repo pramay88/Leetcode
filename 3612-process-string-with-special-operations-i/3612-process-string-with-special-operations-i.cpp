6006d2e4cd118a900dcd4004a89700faa7b8da2d
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-06-16 10:21:00 +0530

    Time: 4 ms (63.64%), Space: 39.9 MB (24.24%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res = "";
+        for(char c: s){
+            if(c == '*'){
+                if(res.size() > 0)
+                    res.pop_back();
+            }
+            else if(c == '#') res += res;
+            else if(c == '%') reverse(res.begin(), res.end());
+            else res += c;
+        }
+        return res;
+    }
+};
