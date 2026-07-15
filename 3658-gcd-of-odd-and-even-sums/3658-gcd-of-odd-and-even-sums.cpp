15137131528d16c4ae5575daa7b63ff2a2413a3c
Pramay Wankhade <155170073+pramay88@users.noreply.github.com>
2026-07-15 10:44:54 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int odd = 0, even = 0;
-        for(int i = 1; i <= n; i++){
-            odd += (2 * i - 1);
-            even += (2 * i);
-        }
-        
-        int gcd = 1;
-        for(int i = 1; i <= min(odd, even); i++){
-            if(odd % i == 0 && even % i == 0)
-                gcd = i;
-        }
+        int odd = n * n;
+        int even = n * (n + 1);
 
-        return gcd;
+        return std::gcd(odd, even);
     }
 };
